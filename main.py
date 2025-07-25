b8de92619c3d87d50605d9d17dc15d6d32fe6e3c
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-07-25 00:19:19 -0600

    comenzando app

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from cuenta_bancaria import CuentaBancaria
 
 mi_cuenta_bancaria = CuentaBancaria() #Instancia de CuentaBancaria creada
 
-print(f"El saldo actual de la cuenta es: {mi_cuenta_bancaria.saldo_inicial}")
+print(f"El saldo actual de la cuenta es: {mi_cuenta_bancaria.saldo}")
 
 mi_cuenta_bancaria.asignar_tarjeta()
 
-print(mi_cuenta_bancaria.tarjeta_vinculada)
+print(mi_cuenta_bancaria.tiene_tarjeta)
